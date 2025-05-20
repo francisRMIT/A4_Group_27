53fa41675b3aa4a2c47ecd4137a07da0bd7d7d81
Francis Servinio <s4005418@student.rmit.edu.au>
2025-05-20 14:01:48 +1000

    Finished AddPerson function

--- Main.java
+++ Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        // Just run main to check if the function works for now
         Person p1 = new Person();
-        p1.setDetails("56s_1d&fAB", "John", "Doe", "32|Highland Street|Melbourne|Victoria|Australia", "12-31-2025");
+        p1.setDetails("56s_ad&fAB", "John", "Doe", "32|Highland Street|Melbourne|Victoria|Australia", "27-02-2025");
 
         boolean condition = p1.addPerson();
         System.out.print(condition);
--- Person.java
+++ Person.java
@@ -1,5 +1,10 @@
 import java.util.HashMap;
 import java.util.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Person {
     public String personID;
@@ -7,10 +12,12 @@ public class Person {
     private String lastName;
     private String address;
     private String birthdate;
+    // Assignment says to use date, but i think it would be better to use
+    // localdate
     private HashMap<Date, Integer> demeritPoints;
     private boolean isSuspended;
 
-    // Temporary class to set details, can change later
+    // Class to set details, can change later
     public void setDetails(String ID, String first, String last, String address, String birthdate) {
         this.personID = ID;
         this.firstName = first;
@@ -20,20 +27,20 @@ public class Person {
     }
 
     public boolean addPerson() {
-
-        // Condition 1: Exactly 10 characters long
+        // Condition 1.0: Exactly 10 characters long
         if (personID.length() != 10) {
             return false;
         }
 
-        // Condition 1: Checking person ID contents
+        // Checking person ID's contents
         int count = 0;
-
         for (int i = 0; i < personID.length(); ++i) {
             // Check if first two chars are digits
             if (i < 2 && !Character.isDigit(personID.charAt(i))) {
                 // If first two are not digits, fails
                 return false;
+                // NOTE: I asked a tutor about whitespaces and he told me to assume that there
+                // will be none
             } else if (!Character.isLetterOrDigit(personID.charAt(i))) {
                 // Counts number of special characters (#$@%! etc.)
                 count += 1;
@@ -45,6 +52,48 @@ public class Person {
             return false;
         }
 
+        // Condition 2.0: Address checking
+        String[] addressSplit = address.split("\\|");
+
+        // If not length 5, then it is not in the correct format
+        if (addressSplit.length != 5) {
+            return false;
+        }
+
+        // Check if Street number is an int
+        try {
+            Integer.parseInt(addressSplit[0]);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+
+        // If the state is not Victoria or the Country is not Australia, fails the
+        // check.
+        if (!addressSplit[3].equals("Victoria") || !addressSplit[4].equals("Australia")) {
+            return false;
+        }
+
+        // Condition 3.0: Parses birthdate and checks if it follows the pattern
+        try {
+            LocalDate.parse(birthdate, DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+        } catch (DateTimeParseException e) {
+            return false;
+        }
+
+        // If everything passes, write down these details into a
+        try {
+            FileWriter addPersonFile = new FileWriter("addPerson.txt");
+            addPersonFile.write("ID: " + personID + "\n");
+            addPersonFile.write("First Name: " + firstName + "\n");
+            addPersonFile.write("Last Name: " + lastName + "\n");
+            addPersonFile.write("Address: " + address + "\n");
+            addPersonFile.write("Birthdate: " + birthdate);
+            addPersonFile.close();
+        } catch (IOException e) {
+            System.out.println("File Error.");
+        }
+
+        // Returns true if all conditions are met
         return true;
     }
 
