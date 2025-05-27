34fd14fc789c7ed34fc8d354aba99de71d8bf727
Francis Servinio <s4005418@student.rmit.edu.au>
2025-05-27 10:50:00 +1000

    Test unit

---
+++ src/Test/UnitTest.java
@@ -0,0 +1,17 @@
+package src.Test;
+
+import src.Main.Person;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions;
+
+public class UnitTest {
+    Person p1 = new Person();
+
+    void testUnit() {
+        assertEquals(true,
+                p1.addPerson("56s_ad&fAB", "John", "Doe", "32|Highland Street|Melbourne|Victoria|Australia",
+                        "27-02-2005"));
+    }
+
+}
